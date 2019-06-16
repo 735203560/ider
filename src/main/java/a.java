21975c92a00f47d8bbd52c542140a98fd7f685a3
Comde <735203560@qq.com>
2019-06-16 20:42:26 +0800

    a ,c

---
+++ src/main/java/a.java
@@ -0,0 +1,10 @@
+public class a {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+        System.out.println("嘿嘿");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+        System.out.println("哈哈");
+    }
+}
